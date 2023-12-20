5bf48041f92fd7da61539b2ab6e64024b06e2ec2
farid <farid.mouzoun@outlook.fr>
2023-12-20 09:39:14 +0100

    exo12
    fini

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+
+var tour = parseInt(prompt("Combien de tour de manège voulez-vous faire ?"));
+for (i = 1 ; i <= tour ; i++ ) {
+    document.write("C'est le tour de manège n°" + i + "<br/>");
+}
